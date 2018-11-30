f946768d926f6cb9f7a41a64afc1b2f5913a19b2
Frankie Delos Santos <frankie@swuser-129-8-201-140.swlan.csufresno.edu>
2018-11-29 17:38:57 -0800

    finished exam

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -21,6 +21,7 @@ export interface ITest {
 export class TestScoreComponent implements OnInit {
 
   tests: Array<ITest> = [];
+  params: string;
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -29,7 +30,65 @@ export class TestScoreComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.tests = await this.loadTests();
 
+  }
+
+  async loadTests() {
+    let tests = JSON.parse(localStorage.getItem('tests'));
+    if (tests && tests.length > 0) {
+      // contacts = contacts;
+    } else {
+      tests = await this.loadTestsFromJson();
+    }
+    console.log('this.contacts from ngOnInit .... ', this.tests);
+    this.tests = tests;
+    return tests;
+
+  }
+
+  async loadTestsFromJson(){
+    const tests = await this.http.get('assests/tests.son').toPromise();
+    return tests.json();
+  }
+
+  addTest() {
+    const test: ITest = {
+      id: null,
+      testName: null,
+      pointsPossible: null,
+      pointsReceived: null,
+      percentage: null,
+      grade: null
+    }
+    this.tests.unshift(test);
+    localStorage.setItem('tests,', JSON.stringify(this.tests));
+  }
+
+  deleteItem(index: number) {
+    this.tests.splice(index, 1);
+    localStorage.setItem('tests', JSON.stringify(this.tests));
+
+  }
+  
+  saveToLocalStorage(){
+    localStorage.setItem('tests', JSON.stringify(this.tests));
+  
+  }
+
+  calculate (){
+    
+
+  }
+  computeGrade(){
+    const data = this.calculate();
+    this.router.navigate(['home', data]);
+    }
+
+  searchBar(params: string){
+    this.tests = this.tests.filter((test: ITest) => {
+      return test.testName.toLocaleLowerCase() === params.toLocaleLowerCase();
+    })
   }
 
 }
